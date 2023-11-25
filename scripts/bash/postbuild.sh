677733cc232bda57ca1e884a965abc307c5226bd
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2023-11-25 13:58:05 -0600

    Instead of scraping SQL, execute it via REDCap

--- scripts/bash/postbuild.sh
+++ scripts/bash/postbuild.sh
@@ -9,45 +9,17 @@ echo "hello from postbuild.sh"
 
 ####################################################################################
 #
-# Install some utilities for REDCap to work properly
+# Call the install.php file with the option to deploy the database schema
 #
 ####################################################################################
 
+curl -sS https://$WEBSITE_HOSTNAME/install.php?auto=1
 
-
 ####################################################################################
 #
-# Install Python3 modules used to scrape REDCap installation SQL script
-#
-####################################################################################
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3
-python3 -m pip install beautifulsoup4
-python3 -m pip install requests
-####################################################################################
-#
-# Scrape the install.php page for SQL commands to execute
-#
+# Update additional configuration settings including
+# user file uploading settings to Azure Blob Storage
+# 
 ####################################################################################
 
-cat << EOF > scraper.py
-import requests
-from bs4 import BeautifulSoup
-page = requests.post("https://$WEBSITE_HOSTNAME/install.php")
-soup = BeautifulSoup(page.content, "html.parser")
-data = soup.find('textarea').text
-with open("/home/install.sql", "w") as out:
-  for i in range(0, len(data)):
-    try:
-      out.write(data[i])
-    except Exception:
-      1+1
-EOF
-python3 scraper.py
-echo "completed running scraper.py with $?"
-####################################################################################
-#
-# Copy the install.sh file to the /home directory
-#
-####################################################################################
-
-cp /home/site/repository/scripts/bash/install.sh /home/install.sh
+bash /home/site/repository/scripts/bash/install.sh
