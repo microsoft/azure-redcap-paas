e638a073e4f48df621120b76c4937e6570c4f30c
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2024-11-07 11:50:08 -0600

    Add `export` statements for cron env vars

--- scripts/bash/startup.sh
+++ scripts/bash/startup.sh
@@ -16,5 +16,12 @@ apt-get update -qq && apt-get install cron sendmail -yqq
 #
 ####################################################################################
 
+# Export environment variables so cron can run cron.php successfully
+export APPSETTING_DBUserName=$APPSETTING_DBUserName
+export APPSETTING_DBHostName=$APPSETTING_DBHostName
+export APPSETTING_DBPassword=$APPSETTING_DBPassword
+export APPSETTING_DBName=$APPSETTING_DBName
+export APPSETTING_DBSslCa=$APPSETTING_DBSslCa
+
 service cron start
 (crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/php /home/site/wwwroot/cron.php > /dev/null")|crontab 
